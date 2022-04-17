8f8bb2a68d5407529b01dd95444ab2de77ee39f9
gromov <gromovitaly@gmail.com>
2022-04-17 20:00:53 +0300

    Initial Commit

---
+++ TipsCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipsCounter
+//
+//  Created by Vitaly Gromov on 4/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipsCounter/TipsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsCounterApp.swift
+//  TipsCounter
+//
+//  Created by Vitaly Gromov on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct TipsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
